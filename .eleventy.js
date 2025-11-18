d3f586b22bb006e2144de4570b1b6ffc70048299
marcofernicola86-arch <marcofernicola86@gmail.com>
2025-11-18 02:15:29 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,5 @@
-// .eleventy.js
 module.exports = function (eleventyConfig) {
-
-  // Copia la cartella admin così com'è
   eleventyConfig.addPassthroughCopy("src/admin");
-
-  // Copia le immagini se in futuro ne hai
-  eleventyConfig.addPassthroughCopy("src/images");
 
   return {
     dir: {
